b7e7c2e056d47101327f18b020a225e2a91270e7
AhmedMohamed365 <ahmedmfreelance@gmail.com>
2022-04-27 17:28:40 +0200

    divding work on N machines works v0.8

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -17,7 +17,6 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 {
 	int* input;
 
-
 	int OriginalImageWidth, OriginalImageHeight;
 
 	//*********************************************************Read Image and save it to local arrayss*************************	
@@ -52,43 +51,28 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 			}
 
 
-			/*Red[i * BM.Width + j] = c.R;
-			Blue[i * BM.Width + j] = c.B;
-			Green[i * BM.Width + j] = c.G;*/
+
 
 			input[i * BM.Width + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
-			//input[i * BM.Width + j] = ((c.R + c.B + c.G) ); //gray scale value equals the average of RGB values
 
-
-			//cout << input[i * BM.Width + j] << " ";
 		}
 
-		//cout << endl;
+
 	}
 	return input;
 }
 
 
-void createImage(int* image, int width, int height, int index, int rank, int worldsize)
+void createImage(int* image, int width, int height, int index, int rank, int worldsize, int* imageData, int origheigh)
 {
 	System::Drawing::Bitmap MyNewImage(width, height);
+	System::Drawing::Bitmap outputimage(width, origheigh);
 	int* output = new int[(height) * (width)];
 
-	int kernal[3][3] = { {0,-1,0},
-						{-1,4,-1},
-						{0,-1,0} };
-
-	/*width -= 1;
-	height -= 1;*/
-	float result = 0;
-
-	for (int i = (rank * MyNewImage.Height) / worldsize; i < ((rank + 1) * MyNewImage.Height) / worldsize; i++)
+	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < MyNewImage.Width; j++)
 		{
-			//i * OriginalImageWidth + j
-
-			//float kernelResult = 0; 
 			if (i == height || j == width || i == 0 || j == 0)
 			{
 				output[i * width + j] = 0;
@@ -115,34 +99,26 @@ void createImage(int* image, int width, int height, int index, int rank, int wor
 				output[i * width + j] = 255;
 			}
 
-
 			System::Drawing::Color c = System::Drawing::Color::FromArgb(output[i * width + j], output[i * width + j], output[i * width + j]);
 			MyNewImage.SetPixel(j, i, c);
 		}
 	}
+	MPI_Gather(output, width * height - 1, MPI_INT, imageData, width * height, MPI_INT, 0, MPI_COMM_WORLD);
 	if (rank == 0)
 	{
-		MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
-		cout << "result Image Saved " << index << endl;
-	}
-	if (rank == 2)
-	{
-		MyNewImage.Save("..//Data//Output//outputResss" + index + ".png");
-		cout << "result Image Saved " << index << endl;
-	}
-	if (rank == 1)
-	{
-		MyNewImage.Save("..//Data//Output//outputRess" + index + ".png");
-		cout << "result Image Saved " << index << endl;
-	}
-	if (rank == 3)
-	{
-		MyNewImage.Save("..//Data//Output//outputRessss" + index + ".png");
+		for (int i = 0; i < outputimage.Height; i++)
+		{
+			for (int j = 0; j < MyNewImage.Width; j++)
+			{
+				System::Drawing::Color c = System::Drawing::Color::FromArgb(imageData[i * width + j], imageData[i * width + j], imageData[i * width + j]);
+				outputimage.SetPixel(j, i, c);
+			}
+		}
+
+		outputimage.Save("..//Data//Output//outputRes" + index + ".png");
 		cout << "result Image Saved " << index << endl;
 	}
 }
-
-
 int main()
 {
 	int rank = 0, world_size;
@@ -152,41 +128,54 @@ int main()
 
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-
-
-
 	int imageWidth = 4, imageHeight = 4;
 	int start_s, stop_s, TotalTime = 0;
+	int* working;
 	if (rank == 0)
 	{
 		System::String^ imagePath;
 		std::string img;
-		img = "..//Data//Input//try2.jpg";
-
+		img = "..//Data//Input//(328).jpg";
 		imagePath = marshal_as<System::String^>(img);
-		inputImage(&imageWidth, &imageHeight, imagePath);
+		int* imageData = inputImage(&imageWidth, &imageHeight, imagePath);
+		working = new int[(imageWidth) * ((imageHeight / world_size) + 1)];
 
+		for (int i = 1; i < world_size; i++)
+		{
+			MPI_Send(&imageHeight, 1, MPI_INT, i, 1000 + i, MPI_COMM_WORLD);
+			MPI_Send(&imageWidth, 1, MPI_INT, i, 2000 + i, MPI_COMM_WORLD);
+		}
+		for (int i = 0; i < (imageWidth) * ((imageHeight / world_size) + 1); i++)
+			working[i] = imageData[i];
+		for (int i = 1; i < world_size; i++)
+		{
+			if ((imageHeight) % world_size == 0)
+			{
+				MPI_Send(&imageData[(i * (imageHeight / world_size)) * imageWidth], (((imageHeight / world_size) + 1) * imageWidth), MPI_INT, i, i, MPI_COMM_WORLD);
+			}
+		}
+		start_s = clock();
+		createImage(working, imageWidth, (((imageHeight / world_size))), 0, rank, world_size, imageData, imageHeight);
+		stop_s = clock();
+		TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
+		cout << "time: " << TotalTime << endl;
+		free(working);
 	}
-	MPI_Bcast(&imageHeight, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&imageWidth, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	int* imageData = new int[imageHeight * imageWidth];
-	if (rank == 0)
+	if (rank != 0)
 	{
-		System::String^ imagePath;
-		std::string img;
-		img = "..//Data//Input//try2.jpg";
-		imagePath = marshal_as<System::String^>(img);
-		imageData = inputImage(&imageWidth, &imageHeight, imagePath);
+		MPI_Status status;
+		MPI_Recv(&imageHeight, 1, MPI_INT, 0, rank + 1000, MPI_COMM_WORLD, &status);
+		MPI_Recv(&imageWidth, 1, MPI_INT, 0, rank + 2000, MPI_COMM_WORLD, &status);
+		working = new int[(imageWidth) * ((imageHeight / world_size) + 1)];
+		MPI_Recv(working, (((imageHeight / world_size) + 1) * imageWidth), MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
+		start_s = clock();
+		createImage(working, imageWidth, (imageHeight / world_size), 0, rank, world_size, working, imageHeight);
+
+		stop_s = clock();
+		TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
+
+		free(working);
 	}
-	MPI_Bcast(imageData, (imageHeight) * (imageWidth), MPI_INT, 0, MPI_COMM_WORLD);
-	start_s = clock();
-	createImage(imageData, imageWidth, imageHeight, 0, rank, world_size);
-	stop_s = clock();
-	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
-	cout << "time: " << TotalTime << endl;
-	free(imageData);
 	MPI_Finalize();
-	return 0;
 }
 
